2a5456c51053fbe4a7fde1c86d15a38c7e2fda67
vmcampos <vmcampos@vmcampos.com>
2014-07-31 23:52:49 -0700

    Updating to jQM 1.4.3 & jQ 1.11
    First steps to upgrade to latest code.

--- ext.js
+++ ext.js
@@ -1,19 +1,24 @@
-			var startNum = 0;
-			var allNums = [];
-			function numLottoPick(){
-				var numLottoHowMany = document.getElementById("numLottoHowManyInput").value;
-				var numLottoMax = document.getElementById("numLottoMaxInput").value;
-				while(numLottoHowMany > 0) {
-					var newRand = Math.ceil(Math.random() * numLottoMax);
-					allNums.push(newRand);
-					document.getElementById("numLottoMsg").style.display = "block";
-					document.getElementById("numLottoResult").innerHTML = allNums;
-					numLottoHowMany--;
-				}
-			}
-			function resetLotto() {
-				document.getElementById("numLottoHowManyInput").value = "";
-				document.getElementById("numLottoMaxInput").value = "";
-				document.getElementById("numLottoMsg").style.display = "none";
-				document.getElementById("numLottoResult").innerHTML = "";
-			}
+/*  jQ Lucky Lotto Code */
+
+var startNum = 0;
+var allNums = [];
+
+function numLottoPick(){
+	var numLottoHowMany = document.getElementById("numLottoHowManyInput").value;
+	var numLottoMax = document.getElementById("numLottoMaxInput").value;
+
+	while(numLottoHowMany > 0) {
+		var newRand = Math.ceil(Math.random() * numLottoMax);
+		allNums.push(newRand);
+		document.getElementById("numLottoMsg").style.display = "block";
+		document.getElementById("numLottoResult").innerHTML = allNums;
+		numLottoHowMany--;
+	}
+}
+
+function resetLotto() {
+	document.getElementById("numLottoHowManyInput").value = "";
+	document.getElementById("numLottoMaxInput").value = "";
+	document.getElementById("numLottoMsg").style.display = "none";
+	document.getElementById("numLottoResult").innerHTML = "";
+}
