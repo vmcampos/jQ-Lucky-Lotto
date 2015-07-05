3f2c55353775f63a2576691fc877972af11d4ed3
vmcampos <vmcampos@vmcampos.com>
2015-07-04 22:25:11 -0700

    Big updates
    Updated jQuery Mobile versions. Updated every <div> to semantic HTML5.
    Refined the JavaScript algorithm to handle bad input better; to display
    output better.

--- ext.js
+++ ext.js
@@ -1,4 +1,8 @@
-/*  jQ Lucky Lotto Code */
+/* 
+	Name: vmcampos <vmcampos@vmcampos.com>
+	Project: VmC jQ Lucky Lotto
+	Version: 2015.07.04
+*/
 
 var startNum = 0;
 var allNums = [];
@@ -7,18 +11,33 @@ function numLottoPick(){
 	var numLottoHowMany = document.getElementById("numLottoHowManyInput").value;
 	var numLottoMax = document.getElementById("numLottoMaxInput").value;
 
-	while(numLottoHowMany > 0) {
-		var newRand = Math.ceil(Math.random() * numLottoMax);
-		allNums.push(newRand);
+	if(numLottoHowMany == "" || numLottoMax == "" || numLottoHowMany == 0 || numLottoMax == 0) {
 		document.getElementById("numLottoMsg").style.display = "block";
-		document.getElementById("numLottoResult").innerHTML = allNums;
-		numLottoHowMany--;
+		document.getElementById("numLottoResult").style.display = "block";
+		document.getElementById("numLottoMsg").innerHTML = "Select how many you want";
+		document.getElementById("numLottoResult").innerHTML = "Select the maximum number";
+		document.getElementById("numLottoHowManyInput").value = "";
+		document.getElementById("numLottoMaxInput").value = "";
+	} else {
+		while(numLottoHowMany > 0) {
+			var newRand = Math.ceil(Math.random() * numLottoMax);
+			allNums.push(" " + newRand);
+			document.getElementById("numLottoMsg").style.display = "block";
+			document.getElementById("numLottoResult").style.display = "block";
+			document.getElementById("numLottoMsg").innerHTML = "Your lucky numbers are";
+			document.getElementById("numLottoResult").innerHTML = allNums;
+			numLottoHowMany--;
+		}
 	}
+	document.getElementById("numLottoHowManyInput").value = "";
+	document.getElementById("numLottoMaxInput").value = "";
 }
 
 function resetLotto() {
+	allNums = [];
 	document.getElementById("numLottoHowManyInput").value = "";
 	document.getElementById("numLottoMaxInput").value = "";
 	document.getElementById("numLottoMsg").style.display = "none";
+	document.getElementById("numLottoResult").style.display = "none";
 	document.getElementById("numLottoResult").innerHTML = "";
 }
